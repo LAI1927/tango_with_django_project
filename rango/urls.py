3701be612975d452508c98b62adb971a26add02f
LAI1927 <503865526@qq.com>
2021-07-27 19:56:01 +0800

    second commit

---
+++ rango/urls.py
@@ -0,0 +1,14 @@
+# -*- coding=utf-8 -*-
+# @Time : 2021/7/27 19:48
+# @Author : YonglaiZhao
+# @File: urls.py.py
+# @Software: PyCharm
+
+from django.urls import path
+from rango import views
+
+app_name = 'rango'
+
+urlpatterns = [
+    path('', views.index, name='index'),
+]
